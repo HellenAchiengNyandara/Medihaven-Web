f08bb86bb45abb6ee9fa923718f73856cfe32bac
HellenNyandaraAchieng <hachieng116@gmail.com>
2025-03-17 11:00:58 +0300

    progress update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import MedicinesDoctors from "./components/medicineDoctors";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col ">
     
       <main className="flex-grow">
         <HeroSection />
